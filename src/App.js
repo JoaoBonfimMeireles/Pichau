daa702b72ebad55f99527da328abad1f8949d35e
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-03-20 01:35:59 -0300

    usando o map e useState com array

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import Name from "./componentes/Name";
 import { useState } from "react";
 
 function App() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [idade, setIdade] = useState("");
-
-
-  const [user, setUser] = useState({})
+    const [input, setInput] = useState("");
+    const [tarefas, setTarefas] = useState([
+        "pagar a conta de luz",
+        "estudar react js"
+    ]);
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    alert("oi")
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })
+    setTarefas([...tarefas, input])
+    setInput("");
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label>Nome:</label><br/>
+        <label>Nome da tarefa:</label><br/>
         <input 
         placeholder="Digite o Nome"
-        value={nome}
-        onChange={ (e) => setNome(e.target.value) }></input><br/>
-
-        <label>Email:</label><br/>
-        <input 
-        placeholder="Digite o Email"
-        value={email}
-        onChange={ (e) => setEmail(e.target.value) }></input><br/>
-
-        <label>Idade:</label><br/>
-        <input 
-        placeholder="Digite o Idade"
-        value={idade}
-        onChange={ (e) => setIdade(e.target.value) }></input><br/><br/>
+        value={input}
+        onChange={ (e) => setInput(e.target.value) }></input><br/>
 
         <button type="submit">Registrar-se</button>
       </form>
-
       <br/><br/>
-      
       <div>
-        <span>Bem vindo: {user.nome}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>Email: {user.email}</span>
+        {tarefas.map( tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+        ))}
       </div>
     </div>
   );
